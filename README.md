16730e0905e5031c477991bd0416886bc7358eec
BryanGK <bryangordonkrauss@gmail.com>
2021-02-18 11:20:05 -0800

    Final refactoring and organizing suggested by Ahmed @ SDMM

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# pomodoro
+# JavaScript Pomodoro Timer
 
-Feb 16, 2021
+### A Pomodoro Timer build with HTML, CSS, and JavaScript 
 
-Application #3 Pomodoro Timer - Software Development Mastermind.
+### See it [here](https://bryangk.github.io/pomodoro/)
 
-A timer to increase productivity by timing work seesions and break sessions into 25min and 5min timers respectively.
+![](pomodoro.png)
+
+## Summary
+A timer to increase productivity by breaking your workday into 25min work and 5min break periods. Part of the base projects in SDMM. This one got me familiar with making countdown timers, manipulating the DOM, adding event listeners, and practicing some CSS styling. I added the extra dropdown menu for selecting a different work/break timer.
+
+## Author
+
+Bryan Krauss - Full Stack Software Developer [LinkedIn](https://www.linkedin.com/in/bryan-krauss-556b3a200/)
--- script.js
+++ script.js
@@ -1,156 +1,154 @@
-const minWork = document.querySelector('.minutes-work');
-const secWork = document.querySelector('.seconds-work');
-const startWork = document.querySelector('.start-work');
-const pauseWork = document.querySelector('.pause-work');
-const resetWork = document.querySelector('.reset-work');
-const selectTime = document.querySelector('#time-selector');
-
-const minBreak = document.querySelector('.minutes-break');
-const secBreak = document.querySelector('.seconds-break');
-const startBreak = document.querySelector('.start-break');
-const pauseBreak = document.querySelector('.pause-break');
+let workTimer;
+let breakTimer;
 
 let workTime = {
-    minutes: 0,
-    seconds: 0,
+  minutes: 0,
+  seconds: 0,
 };
 
 let breakTime = {
-    minutes: 0,
-    seconds: 0,
+  minutes: 0,
+  seconds: 0,
 };
 
-let workAudio = new Audio('/sounds/work.mp3')
-let breakAudio = new Audio('/sounds/break.mp3');
+const workAudio = new Audio("/sounds/work.mp3");
+const breakAudio = new Audio("/sounds/break.mp3");
 
-displayTime();
-initialState();
+const minWork = document.querySelector(".minutes-work");
+const secWork = document.querySelector(".seconds-work");
+
+const minBreak = document.querySelector(".minutes-break");
+const secBreak = document.querySelector(".seconds-break");
+
+const startWork = document.querySelector(".start-work");
+startWork.addEventListener("click", () => {
+  workTimer = setInterval(() => countDownWork(workTime), 1000);
+  selectTime.disabled = true;
+  startWork.disabled = true;
+  resetWork.disabled = true;
+  return workTimer;
+});
+
+const pauseWork = document.querySelector(".pause-work");
+pauseWork.addEventListener("click", () => {
+  if (startWork.disabled) {
+    startWork.disabled = false;
+    resetWork.disabled = false;
+  }
+  clearInterval(workTimer);
+});
+
+const resetWork = document.querySelector(".reset-work");
+resetWork.addEventListener("click", () => {
+  selectTime.disabled = false;
+  initialState();
+  timerValues();
+});
+
+const startBreak = document.querySelector(".start-break");
+startBreak.addEventListener("click", () => {
+  breakTimer = setInterval(() => countDownBreak(breakTime), 1000);
+  selectTime.disabled = true;
+  startBreak.disabled = true;
+  startWork.disabled = true;
+  resetWork.disabled = true;
+  return breakTimer;
+});
+
+const pauseBreak = document.querySelector(".pause-break");
+pauseBreak.addEventListener("click", () => {
+  if (startBreak.disabled) {
+    startBreak.disabled = false;
+    resetWork.disabled = false;
+  }
+  clearInterval(breakTimer);
+});
+
+const selectTime = document.querySelector("#time-selector");
+selectTime.addEventListener("change", timerValues);
 
 function initialState() {
-    startWork.disabled = true;
-    startBreak.disabled = true;
-    pauseWork.disabled = true;
-    pauseBreak.disabled = true;
-    resetWork.disabled = true;
+  startWork.disabled = true;
+  startBreak.disabled = true;
+  pauseWork.disabled = true;
+  pauseBreak.disabled = true;
+  resetWork.disabled = true;
 }
 
 function displayTime() {
-    minWork.innerHTML = `${workTime.minutes}:`;
-    secWork.innerHTML = `0${workTime.seconds}`;
-    minBreak.innerHTML = `${breakTime.minutes}:`;
-    secBreak.innerHTML = `0${breakTime.seconds}`;
+  minWork.textContent = `${workTime.minutes}:`;
+  secWork.textContent = `0${workTime.seconds}`;
+  minBreak.textContent = `${breakTime.minutes}:`;
+  secBreak.textContent = `0${breakTime.seconds}`;
 }
 
-selectTime.addEventListener('change', timerValues)
-
 function timerValues() {
-    if (selectTime.value === '25-5') {
-        workTime.minutes = 25;
-        workTime.seconds = 0;
-        breakTime.minutes = 5;
-        breakTime.seconds = 0;
-        displayTime();
-    }
-    if (selectTime.value === '45-15') {
-        workTime.minutes = 45;
-        workTime.seconds = 0;
-        breakTime.minutes = 15;
-        breakTime.seconds = 0;
-        displayTime();
-    }
-    startWork.disabled = false;
-    pauseWork.disabled = false;
-    resetWork.disabled = false;
+  if (selectTime.value === "25-5") shortPomodoro();
+
+  if (selectTime.value === "45-15") longPomodoro();
+
+  displayTime();
+  startWork.disabled = false;
+  pauseWork.disabled = false;
+  resetWork.disabled = false;
+}
+
+function longPomodoro() {
+  workTime.minutes = 45;
+  workTime.seconds = 0;
+  breakTime.minutes = 15;
+  breakTime.seconds = 0;
+}
+
+function shortPomodoro() {
+  workTime.minutes = 25;
+  workTime.seconds = 0;
+  breakTime.minutes = 5;
+  breakTime.seconds = 0;
 }
 
 function countDownWork(time) {
-    if (time.minutes === 0 && time.seconds === 1) {
-        clearInterval(workTimer);
-        workAudio.play();
-        startBreak.disabled = false;
-        pauseBreak.disabled = false;
-        pauseWork.disabled = true;
-        resetWork.disabled = false;
-    }
-    if (time.seconds === 0) {
-        time.seconds = 60;
-        time.minutes--;
-    }
-    time.seconds--;
-    if (time.seconds < 10) {
-        secWork.innerHTML = `0${time.seconds}`;
-    } else {
-        secWork.innerHTML = time.seconds;
-    }
-    minWork.innerHTML = `${time.minutes}:`;
-    startWork.disabled = true;
+  if (time.minutes === 0 && time.seconds === 1) {
+    clearInterval(workTimer);
+    workAudio.play();
+    startBreak.disabled = false;
+    pauseBreak.disabled = false;
+    pauseWork.disabled = true;
+    resetWork.disabled = false;
+  }
+  decrementTime(time);
+  time.seconds < 10
+    ? (secWork.textContent = `0${time.seconds}`)
+    : (secWork.textContent = time.seconds);
+
+  minWork.textContent = `${time.minutes}:`;
+  startWork.disabled = true;
+}
+
+function decrementTime(time) {
+  if (time.seconds === 0) {
+    time.seconds = 60;
+    time.minutes--;
+  }
+  time.seconds--;
 }
 
 function countDownBreak(time) {
-    if (time.minutes === 0 && time.seconds === 1) {
-        clearInterval(breakTimer);
-        startBreak.disabled = true;
-        pauseBreak.disabled = true;
-        resetWork.disabled = false;
-        breakAudio.play();
-    }
-    if (time.seconds === 0) {
-        time.seconds = 60;
-        time.minutes--;
-    }
-    time.seconds--;
-    if (time.seconds < 10) {
-        secBreak.innerHTML = `0${time.seconds}`;
-    } else {
-        secBreak.innerHTML = time.seconds;
-    }
-    minBreak.innerHTML = `${time.minutes}:`;
-    startWork.disabled = true;
+  if (time.minutes === 0 && time.seconds === 1) {
+    clearInterval(breakTimer);
+    startBreak.disabled = true;
+    pauseBreak.disabled = true;
+    resetWork.disabled = false;
+    breakAudio.play();
+  }
+  decrementTime(time);
+  time.seconds < 10
+    ? (secBreak.textContent = `0${time.seconds}`)
+    : (secBreak.textContent = time.seconds);
+
+  minBreak.textContent = `${time.minutes}:`;
+  startWork.disabled = true;
 }
 
-// Work Timer //
-
-let workTimer;
-
-startWork.addEventListener('click', () => {
-    workTimer = setInterval(function () { countDownWork(workTime) }, 1000);
-    selectTime.disabled = true;
-    startWork.disabled = true;
-    resetWork.disabled = true;
-    return workTimer;
-});
-
-pauseWork.addEventListener('click', () => {
-    if (startWork.disabled) {
-        startWork.disabled = false;
-        resetWork.disabled = false;
-    }
-    clearInterval(workTimer);
-});
-
-resetWork.addEventListener('click', () => {
-    selectTime.disabled = false;
-    initialState();
-    timerValues();
-});
-
-// Break timer //
-
-let breakTimer;
-
-startBreak.addEventListener('click', () => {
-    breakTimer = setInterval(function () { countDownBreak(breakTime) }, 1000);
-    selectTime.disabled = true;
-    startBreak.disabled = true;
-    startWork.disabled = true;
-    resetWork.disabled = true;
-    return breakTimer;
-});
-
-pauseBreak.addEventListener('click', () => {
-    if (startBreak.disabled) {
-        startBreak.disabled = false;
-        resetWork.disabled = false;
-    }
-    clearInterval(breakTimer);
-});
+displayTime();
+initialState();
